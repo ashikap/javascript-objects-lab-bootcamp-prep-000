86072e083154a688354a8179b9d756d6070224e4
ashikap89@gmail.com <ashikap89@gmail.com>
2018-02-25 22:46:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
    delete object.key;
    return obj;
- }
- 
-  function destructivelyDeleteFromObjectByKey(object, key) {
-   delete object.key
-   return object;
- }
+}
+
+
 
